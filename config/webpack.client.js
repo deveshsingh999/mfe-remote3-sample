894376143f7a64b3a1b24d8250294f68a799f32b
Abhishek Raj <abhishekr@dewsolutions.in>
2023-05-02 00:01:28 +0530

    Setup react

---
+++ config/webpack.client.js
@@ -0,0 +1,72 @@
+require('./configure');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { EnvironmentPlugin } = require('webpack');
+const { isProdFunc } = require('./config-utils');
+
+const isProd = isProdFunc();
+
+const plugins = [
+  new EnvironmentPlugin({
+    // NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
+    MFE_NAME: 'remote',
+    PROD_MODE: 'false',
+    DEV_SERVER: 'false',
+  }),
+  new HtmlWebpackPlugin({
+    template: "./public/index.html",
+    templateParameters: {
+      ...process.env,
+    },
+  }),
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: "public",
+        globOptions: {
+          ignore: ['**/index.html'],
+        },
+        noErrorOnMissing: true,
+      },
+    ],
+  }),
+];
+
+module.exports = {
+  name: "client",
+  mode: isProd ? "production" : "development",
+  entry: "./src/index.jsx",
+  output: {
+    path: path.join(process.cwd(), "dist"),
+    filename: "bundle.js",
+    chunkFilename: '[name].bundle.js',
+    publicPath: process.env.MFE_PUBLIC_PATH || '/',
+  },
+  devtool: isProd ? "source-map" : "cheap-module-source-map",
+  target: 'web',
+  devServer: {
+    static: {
+      directory: path.join(process.cwd(), 'public'),
+    },
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  plugins,
+};
---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+  return (
+    <h1>React App 1</h1>
+  );
+}
+
+export default App;
